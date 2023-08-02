a4e95682fda426f6a428e5e91b21695d4e2186be
andrianoavy <abrahamramilison+andrianoavy@gmail.com>
2023-08-02 17:25:19 +0300

    fix cors

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseApiURL: 'http://localhost:3000/api'
+    // baseApiURL: 'http://localhost:3000/api'
+    baseApiURL: 'https://tourisme-mada-m1p10android.onrender.com/api'
   };
